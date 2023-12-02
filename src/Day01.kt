7396771f0bbe46470e4d3190dad1ede94a94bca7
Karsten Meier <developer@handylearn-projects.de>
2023-12-02 18:01:38 +0100

    day 1 part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,15 @@
+fun String.firstDigit() : Int {
+    return this.first{ ch -> ch in '0'..'9' }.digitToInt()
+}
+fun String.calibrationValue(): Int {
+    return (10 * this.firstDigit() + this.reversed().firstDigit())
+}
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map{
+            it.calibrationValue()
+        }.sum()
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +18,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
     part1(input).println()
